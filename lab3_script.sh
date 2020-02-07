7b6e50db21c1ff41dd4a4a8f6cccbfe7d0ef64a1
Charles <charles@pop-os.localdomain>
2020-02-07 09:19:55 -0700

    Adding all of the files from lab 3

---
+++ lab3_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Authors : Your Name & Your partner's name
+# Date: 1/1/2019
+
+echo "Input File Name: "
+read filename
+echo "Input regular expression: "
+read regex 
+grep -E $regex $filename 
+grep -Ec '[0-9]{3}-[0-9]{3}-[0-9]{4}' $filename
+grep -Ec [@] $filename 
+grep -E '^303' $filename
+grep -E '@geocities.com$' $filename >> email_results.txt
