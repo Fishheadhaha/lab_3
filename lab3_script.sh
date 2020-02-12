2e8d396a686215e9977872fa95425121cf51f0c2
Charles <charles@pop-os.localdomain>
2020-02-12 11:42:07 -0700

    fixed

--- lab3_script.sh
+++ lab3_script.sh
@@ -8,6 +8,6 @@ echo "Input regular expression: "
 read regex 
 grep -E $regex $filename 
 grep -Ec '[0-9]{3}-[0-9]{3}-[0-9]{4}' $filename
-grep -Ec [@] $filename 
+grep -Ec '^[^@]+@.+\..+' $filename 
 grep -E '^303' $filename
 grep -E '@geocities.com$' $filename >> email_results.txt
